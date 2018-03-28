b5ee49a7ddaac6c0eb2e4128cc8a30084cde6222
SpamTester <37859912+SpamTester@users.noreply.github.com>
2018-03-28 11:26:55 +0530

    Add files via upload

---
+++ home.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit  } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { InAppBrowser } from '@ionic-native/in-app-browser'; /* 1 */
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage implements OnInit{
+
+                      constructor(public navCtrl: NavController,private iab: InAppBrowser /* 2 */) {}
+                      ngOnInit(){
+
+                          const browser = this.iab.create('https://www.pokeryaar.com/','_self',{location:'no'}); /*3*/
+
+                      }
+
+}
